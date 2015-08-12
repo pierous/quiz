734be649bad7fd682e516816567c1106194a8c87
pierous <pierous@gmail.com>
2015-08-12 17:50:22 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,8 @@ router.get('/author', function(req, res) {
 });
 
 // Autoload de comandos con :quizId
-router.param('quizId', quizController.load);
+router.param('quizId', quizController.load);		// autoload :quizId
+router.param('commentId', commentController.load);	// autoload :commentId
 
 // Definición de rutas de sesión
 router.get('/login', sessionController.new);		// formulario login
@@ -34,7 +35,8 @@ router.put('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizContr
 router.delete('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizController.destroy);
 
 // Definición de rutas de comentarios
-router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/new', 						commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments', 							commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 	sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
